03a0e988b668b372ecd47bacc4770fb5e47c8511
murphydhee <murphydhee@gmail.com>
2023-09-26 01:55:22 +0100

    home page text

--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -4,7 +4,7 @@
       <div>
         <main-nav-bar />
       </div>
-      <div class="history__content">
+      <div class="history__content__layout">
         <div class="history__header">
           <div>
             <h1 style="font-size: 120px">01.</h1>
@@ -39,22 +39,22 @@
             <h2>Climb</h2>
           </div>
         </div>
-        <div>
-          <p class="c">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+        <div class="history__content">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aute
+            irure dolor in reprehenderit in voluptate velit esse cillum dolore.
           </p>
         </div>
       </div>
     </div>
-    <div class="history__section__three"></div>
+    <div class="menu__tabs"></div>
+    <div class="history__section__three">
+      <div>
+        <schedule />
+      </div>
+    </div>
     <div>
-      <schedule />
+      <FooterBar />
     </div>
   </div>
 </template>
@@ -62,6 +62,7 @@
 import MainNavBar from "@/components/MainNavBar.vue";
 import Carousel from "@/components/Carousel.vue";
 import Schedule from "@/components/Schedule.vue";
+import FooterBar from "@/components/FooterBar.vue";
 </script>
 <style lang="scss" scoped>
 .section__layout {
@@ -77,16 +78,33 @@ import Schedule from "@/components/Schedule.vue";
   flex-direction: column;
   justify-content: space-between;
 }
+.history__section__three {
+  background-image: url("@/assets/image5.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
 .history__header {
   display: flex;
   justify-self: flex-start;
 }
-.history__content {
+.history__content__two {
+  display: flex;
+  gap: 1em;
+}
+.history__content__layout {
   display: flex;
   position: absolute;
   margin-top: 1em;
   padding: 3em 15em;
   flex-direction: column;
+}
+.history__content {
+  display: flex;
+  align-self: center;
 }
 .history__section__two {
   display: flex;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,10 @@
     <div>
       <nav-bar />
     </div>
-    <p>Home View</p>
+    <div class="header__text__layout">
+      <h1 class="header__text__one">LOS ANGELES</h1>
+      <h1 class="header__text__two">MOUNTAINS</h1>
+    </div>
   </div>
   <div>
     <footer-bar />
@@ -19,5 +22,20 @@ import FooterBar from "@/components/FooterBar.vue";
   background-size: cover;
   background-repeat: no-repeat;
   height: 100vh;
+}
+.header__text__layout {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  place-items: center;
+  height: 100vh;
+
+  .header__text__one {
+    font-size: 85px;
+  }
+  .header__text__two {
+    font-size: 85px;
+  }
 }
 </style>
