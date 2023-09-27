29309c2bbe2c99d0e30bac45c4907c063a7c162e
murphydhee <murphydhee@gmail.com>
2023-09-27 04:04:45 +0100

    tabs build

---
+++ src/components/MountainTabs.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div
+      class="tabcontent"
+      v-for="tab in tabs"
+      :key="tab.name"
+      :id="tab.name"
+      v-show="tab.active"
+    >
+      <!-- Content for each tab -->
+    </div>
+    <button
+      class="tablinks"
+      v-for="tab in tabs"
+      :key="tab.name"
+      @click="openTab(tab)"
+      :class="{ active: tab.active }"
+    >
+      {{ tab.name }}
+    </button>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const tabs = ref([
+      { name: "Tab1", active: true },
+      { name: "Tab2", active: false },
+      { name: "Tab3", active: false },
+      // Add more tabs as needed
+    ]);
+
+    const openTab = (selectedTab) => {
+      tabs.value.forEach((tab) => {
+        tab.active = tab === selectedTab;
+      });
+    };
+
+    return {
+      tabs,
+      openTab,
+    };
+  },
+};
+</script>
+
+<style scoped>
+/* Your CSS styles for tabcontent and tablinks */
+</style>
--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -1,9 +1,25 @@
 <template>
   <div class="schedule__card">
     <h2>Schedule</h2>
-    <div class="event__details">
-      <p>25 Nov 2016</p>
-      <p>Vestibibrum vviera</p>
+    <div class="schedule__details">
+      <div class="event__details">
+        <p>25 Nov 2016</p>
+        <p>Vestibibrum vviera</p>
+      </div>
+      <div class="event__details">
+        <p>25 Nov 2016</p>
+        <p>Vestibibrum vviera</p>
+      </div>
+    </div>
+    <div class="schedule__details__two">
+      <div class="event__details">
+        <p>25 Nov 2016</p>
+        <p>Vestibibrum vviera</p>
+      </div>
+      <div class="event__details">
+        <p>25 Nov 2016</p>
+        <p>Vestibibrum vviera</p>
+      </div>
     </div>
   </div>
 </template>
@@ -16,10 +32,22 @@
   gap: 1em;
   border: 1px solid red;
   width: 40%;
+  background-color: whitesmoke;
+  opacity: 50%;
 }
 .event__details {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+}
+.schedule__details {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+}
+.schedule__details__two {
+  display: flex;
+  flex-direction: column;
+  gap: 1.5em;
 }
 </style>
